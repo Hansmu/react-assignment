d06e80a91a7a79a2d62a1ec9029f6bdd0df4d857
hansmu <hans.miikael@gmail.com>
2017-07-20 22:38:37 +0300

    Saving state to and from file

--- frontend/src/containers/nodes-view.js
+++ frontend/src/containers/nodes-view.js
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import Dropzone from 'react-dropzone'
 import { Glyphicon, Button, Row } from 'react-bootstrap';
+import FileSaver from 'file-saver';
 
-import { updateNodes, saveStateToLocalStorage, loadStateFromLocalStorage } from '../actions';
+import { updateNodes, saveStateToLocalStorage, loadStateFromLocalStorage, saveStateToFile, loadStateFromFile } from '../actions';
 
 class NodesView extends Component {
 
@@ -16,6 +18,7 @@ class NodesView extends Component {
             nodeName: ''
         };
 
+        this.onDrop = this.onDrop.bind(this);
         this.addNode = this.addNode.bind(this);
         this.renderNode = this.renderNode.bind(this);
         this.findAndModifyNode = this.findAndModifyNode.bind(this);
@@ -168,6 +171,23 @@ class NodesView extends Component {
         );
     }
 
+    onDrop(files) {
+        const file = files[0];
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const nodes = JSON.parse(event.target.result);
+            this.props.dispatch(updateNodes(nodes));
+        };
+
+        reader.readAsText(file);
+    }
+
+    saveFile() {
+        const nodes = JSON.stringify(this.props.nodes)
+        const blob = new Blob([nodes], {type: 'text/plain;charset=utf-8'});
+        FileSaver.saveAs(blob, 'nodes.txt');
+    }
+
     render() {
         return (
             <div>
@@ -176,6 +196,18 @@ class NodesView extends Component {
                 </Button>
                 <Button onClick={() => this.props.dispatch(loadStateFromLocalStorage())}>
                     Load From Local Storage
+                </Button>
+                <Button onClick={() => this.saveFile()}>
+                    Save To File
+                </Button>
+                <div className="dropzone">
+                    <Dropzone multiple={false} onDrop={this.onDrop.bind(this)}>
+                        <p>Try dropping some files here, or click to select files to upload.</p>
+                    </Dropzone>
+                </div>
+
+                <Button onClick={() => this.props.dispatch(loadStateFromFile())}>
+                    Load From File
                 </Button>
 
                 <br/>
