53a97c90c9dbad0face640903e4bd5d07299c3f8
hansmu <hans.miikael@gmail.com>
2017-07-21 13:15:22 +0300

    Remove backend folder as server communication is mocked

--- backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-MERGE INTO example (id, first_name, last_name) VALUES (1, 'John Johnson', 'McJohn');
-MERGE INTO example (id, first_name, last_name) VALUES (2, 'Michael', 'McMichaelsen');
-MERGE INTO example (id, first_name, last_name) VALUES (3, 'Sir Jonathan Michael JWizzle The Drizzle', 'Lastname the Third');
--- backend/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS example;
-
-CREATE TABLE example
-(
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  first_name VARCHAR2(250),
-  last_name VARCHAR2(250)
-);
