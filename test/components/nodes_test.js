12ed85d531b4bef7bf391a75b197f3c0909bb868
hansmu <hans.miikael@gmail.com>
2017-07-22 23:45:02 +0300

    Fix tests

--- test/components/nodes_test.js
+++ test/components/nodes_test.js
@@ -22,9 +22,9 @@ describe('Nodes View', () => {
         component.find('#modify-field').simulate('click');
 
         expect(component.find('a').length).to.equal(3);
-        expect(component.find('#node-name-0')).to.have.text(' bob ');
-        expect(component.find('#node-name-1')).to.have.text(' richard ');
-        expect(component.find('#node-name-0-0')).to.have.text(' 1-2-3 ');
+        expect(component.find('#node-name-0')).to.have.text('bob ');
+        expect(component.find('#node-name-1')).to.have.text('richard ');
+        expect(component.find('#node-name-0-0')).to.have.text('1-2-3 ');
     });
 
     describe('Edit elements', () => {
@@ -38,18 +38,18 @@ describe('Nodes View', () => {
             component.find('#delete-child-0').simulate('click');
 
             expect(component.find('a').length).to.equal(1);
-            expect(component.find('#node-name-0')).to.have.text(' richard ');
+            expect(component.find('#node-name-0')).to.have.text('richard ');
         });
 
         it('Edit child', () => {
-            expect(component.find('#node-name-0-0')).to.have.text(' 1-2-3 ');
+            expect(component.find('#node-name-0-0')).to.have.text('1-2-3 ');
 
             component.find('#edit-child-0-0').simulate('click');
             component.find('#node-name-field').simulate('change', 'middle-node');
             component.find('#modify-field').simulate('click');
 
             expect(component.find('a').length).to.equal(4);
-            expect(component.find('#node-name-0-0')).to.have.text(' middle-node ');
+            expect(component.find('#node-name-0-0')).to.have.text('middle-node ');
         });
     });
 
