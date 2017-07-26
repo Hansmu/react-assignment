18e57d8cf05e6a7b23fb09bf1ce9192813e6ccb0
hansmu <hans.miikael@gmail.com>
2017-07-26 17:45:11 +0300

    Remove coverage reporter and set PhantomJS as the browser to test on.

--- karma.conf.js
+++ karma.conf.js
@@ -2,43 +2,33 @@ var webpack = require('webpack');
 
 module.exports = function (config) {
     config.set({
-        browsers: [ 'Chrome' ], //run in Chrome
-        singleRun: true, //just run once by default
-        frameworks: [ 'mocha' ], //use the mocha test framework
+        browsers: [ 'PhantomJS' ],
+        singleRun: true,
+        frameworks: [ 'mocha' ],
         files: [
-            'tests.webpack.js' //just load this file
+            'tests.webpack.js'
         ],
-        plugins: [ 'karma-chrome-launcher', 'karma-chai', 'karma-mocha',
+        plugins: [ 'karma-phantomjs-launcher', 'karma-chai', 'karma-mocha',
             'karma-sourcemap-loader', 'karma-webpack', 'karma-coverage',
             'karma-mocha-reporter'
         ],
         preprocessors: {
-            'tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+            'tests.webpack.js': [ 'webpack', 'sourcemap' ]
         },
-        reporters: [ 'mocha', 'coverage' ], //report results in this format
-        webpack: { //kind of a copy of your webpack config
-            devtool: 'inline-source-map', //just do inline source maps instead of the default
+        reporters: ['mocha'],
+        webpack: {
+            devtool: 'inline-source-map',
             module: {
                 rules: [
                     {
                         test: /\.js$/,
                         use: 'babel-loader'
-                    },
-                    {
-                        enforce: 'post',
-                        test: /\.js$/,
-                        exclude: /(test|node_modules)\//,
-                        use: 'istanbul-instrumenter-loader'
                     }
                 ]
             }
         },
         webpackServer: {
-            noInfo: true //please don't spam the console when running in karma!
-        },
-        coverageReporter: {
-            type: 'html', //produces a html document after code is run
-            dir: 'coverage/' //path to created html doc
+            noInfo: true
         }
     });
 };
