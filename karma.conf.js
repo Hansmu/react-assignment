73f4fbf5447a3f1835abdaad3abd5d7f74e9cf16
hansmu <hans.miikael@gmail.com>
2017-07-27 23:54:26 +0300

    Keep Karma running by default

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = function (config) {
     config.set({
         browsers: [ 'Chrome' ],
-        singleRun: true,
+        singleRun: false,
         frameworks: [ 'mocha', 'chai', 'sinon'],
         files: [
             'tests.webpack.js'
