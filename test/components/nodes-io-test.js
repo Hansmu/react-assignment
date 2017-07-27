3b927bf66bfd46593bda4330be8b6b3d812b2213
hansmu <hans.miikael@gmail.com>
2017-07-27 22:36:26 +0300

    File upload test

--- test/components/nodes-io-test.js
+++ test/components/nodes-io-test.js
@@ -6,16 +6,19 @@ describe('Nodes IO', () => {
     let component;
 
     const defaultState = [{"name":"bob","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}];
+    const defaultStateAsString = '[{"name":"bob","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]';
+
     const modifiedStateAsString = '[{"name":"china","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]';
     const modifiedState = [{"name":"china","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}];
 
     beforeEach(() => {
+        window.localStorage.setItem('nodes', []);
+
         const props = { main: {nodes:  defaultState}};
         component = renderComponent(NodesIO, null, props);
     });
 
     it('Save to local storage', () => {
-        window.localStorage.setItem('nodes', []);
         component.find('#local-storage-save').simulate('click');
 
         assert(window.localStorage.getItem('nodes'), defaultState);
@@ -28,6 +31,18 @@ describe('Nodes IO', () => {
         component.find('#local-storage-save').simulate('click');
 
         assert(window.localStorage.getItem('nodes'), modifiedState);
+    });
+
+    it('Load state from local file', () => {
+        component = renderComponent(NodesIO);
+
+        const fileContents = { fileContents: defaultStateAsString };
+        const file = new Blob([fileContents], { type: 'text/plain' });
+
+        component.find('input').simulate('change', {target: {files: [file]}});
+        component.find('#local-storage-save').simulate('click');
+
+        assert(window.localStorage.getItem('nodes'), defaultState);
     });
 
     it('Save to and load from server', () => {
