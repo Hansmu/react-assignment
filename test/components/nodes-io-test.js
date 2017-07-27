7801a15b7d7e72117f4511a4dc07426b49013618
hansmu <hans.miikael@gmail.com>
2017-07-27 21:59:11 +0300

    File saving test from server. Add middleware to keep promise flow

--- test/components/nodes-io-test.js
+++ test/components/nodes-io-test.js
@@ -5,8 +5,12 @@ import NodesIO from '../../src/containers/nodes-io';
 describe('Nodes IO', () => {
     let component;
 
+    const defaultState = [{"name":"bob","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}];
+    const modifiedStateAsString = '[{"name":"china","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]';
+    const modifiedState = [{"name":"china","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}];
+
     beforeEach(() => {
-        const props = { main: {nodes:  [{"name":"bob","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]}};
+        const props = { main: {nodes:  defaultState}};
         component = renderComponent(NodesIO, null, props);
     });
 
@@ -14,15 +18,27 @@ describe('Nodes IO', () => {
         window.localStorage.setItem('nodes', []);
         component.find('#local-storage-save').simulate('click');
 
-        assert(window.localStorage.getItem('nodes'), [{"name":"bob","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]);
+        assert(window.localStorage.getItem('nodes'), defaultState);
     });
 
     it('Save and load from local storage', () => {
-        window.localStorage.setItem('nodes', '[{"name":"china","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]');
+        window.localStorage.setItem('nodes', modifiedStateAsString);
         component.find('#local-storage-load').simulate('click');
 
         component.find('#local-storage-save').simulate('click');
 
-        assert(window.localStorage.getItem('nodes'), [{"name":"china","children":[{"name":"1-2-3","children":[{"name":"5-67-","children":[],"isOpen":true}],"isOpen":true}],"isOpen":true},{"name":"richard","children":[],"isOpen":true}]);
+        assert(window.localStorage.getItem('nodes'), modifiedState);
+    });
+
+    it('Save to and load from server', () => {
+        component.find('#remote-save').simulate('click');
+
+        window.localStorage.setItem('nodes', '[]');
+        component.find('#local-storage-load').simulate('click');
+
+        component.find('#remote-load').simulate('click');
+        component.find('#local-storage-save').simulate('click');
+
+        assert(window.localStorage.getItem('nodes'), defaultState);
     });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from '../src/reducers';
 import { Provider } from 'react-redux';
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
     return mount(
-        <Provider store={createStore(reducers, state)}>
+        <Provider store={createStoreWithMiddleware(reducers, state)}>
           <ComponentClass {...props} />
         </Provider>
     );
